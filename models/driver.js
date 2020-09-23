6c2cf07681750c543a2b6d87aa63a7c5529fa206
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-23 12:08:34 +0200

    added Driver model

---
+++ models/driver.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const DriverSchema = new Schema({
+  email: { type: String, required: true },
+  driving: { type: Boolean, default: false },
+  // location:
+});
+
+const Driver = mongoose.model('driver', DriverSchema);
+
+module.exports = Driver;
