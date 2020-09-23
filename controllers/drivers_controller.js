ee12660668b5bde48836c881ff991da079170682
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-23 12:12:46 +0200

    added create route and controller

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose');
+
 module.exports = {
   greeting: (req, res) => {
     res.send({ hi: 'there' });
+  },
+
+  create: (req, res) => {
+    /* const data = req.body;
+    res.send(); */
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,7 @@ const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
+
+  //# create driver
+  app.post('/api/drivers', DriversController.create);
 };
