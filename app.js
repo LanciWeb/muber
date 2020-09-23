261afe32ba4f796abc2d53eb90c148b7804be94a
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-23 15:14:08 +0200

    connected mongoose database

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const routes = require('./routes');
 const app = express();
+const routes = require('./routes');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
 
 //! always BEFORE app
 app.use(bodyParser.json());
