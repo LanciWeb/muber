010b0ebca9f0947004c786a1a89b5e9f206d2399
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-24 14:41:41 +0200

    added point schema as driver subdocument

--- models/driver.js
+++ models/driver.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const PointSchema = new Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], required: true, index: '2dsphere' },
+});
+
 const DriverSchema = new Schema({
+  geometry: PointSchema,
   email: { type: String, required: true },
   driving: { type: Boolean, default: false },
-  // location:
 });
 
 const Driver = mongoose.model('driver', DriverSchema);
